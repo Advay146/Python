9f8aaa41ee9c9d4a3e4d30fa69c122bd51b7e879
Advay146 <advaygupta146@gmail.com>
2025-06-18 21:19:52 +0530

    Add files via upload

---
+++ Table of 23.py
@@ -0,0 +1,5 @@
+# Python program to print the multiplication table of 234 from 1 to 20
+# Loop form 1-20
+for i in range(1,21):
+    # Print the multiplication of 23 by i
+    print(f"234*{i} = {234*i}" )
