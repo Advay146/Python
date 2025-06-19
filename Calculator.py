bd1a0477a68c168684e0f44e8bf3e955bc9cc22f
Advay146 <advaygupta146@gmail.com>
2025-06-19 21:47:00 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def division(x,y):
+    return x/y
+def multiply(x,y):
+    return x*y
+num1=int(input("Enter number 1 :"))
+num2=int(input("Enter number 2 :"))
+
+print("Sum :",add(num1,num2))
+print("Subtraction :", subtract(num1, num2))
+print("Division :",division(num1,num2))
+print("Multiplication:", multiply(num1, num2))
