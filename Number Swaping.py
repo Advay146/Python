ed7d6cdbca6720c3995cbdbe17f6fbafd0d65e4f
Advay146 <advaygupta146@gmail.com>
2025-06-25 19:09:29 +0530

    Add files via upload

---
+++ Number Swaping.py
@@ -0,0 +1,14 @@
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+# Storing the numbers in a list
+numbers = [x, y, z]
+
+# Sorting the list in ascending order
+numbers.sort()
+
+# Printing the sorted numbers
+print("Numbers in ascending order:", numbers)
+
