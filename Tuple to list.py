45ce4f6aa8b5235913f57d7790e8c74fdb0ae537
Advay146 <advaygupta146@gmail.com>
2025-07-16 20:43:23 +0530

    Add files via upload

---
+++ Tuple to list.py
@@ -0,0 +1,11 @@
+person_info = ("Advay", 14, "Male", 5.4)  
+
+
+person_list = list(person_info)
+
+print("Converted List:", person_list)
+
+print("Name:", person_list[0])
+print("Age:", person_list[1])
+print("Gender:", person_list[2])
+print("Height:", person_list[3], "feet")
